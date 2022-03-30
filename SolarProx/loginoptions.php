0d9851700551d0421bcf47125dd267e0b68c0c19
root <arthurjhammond@gmail.com>
2022-03-30 12:50:29 +0000

    added auth options page

---
+++ SolarProx/loginoptions.php
@@ -0,0 +1,15 @@
+<html>
+
+
+<h1> login options </h1>
+
+<a href="login.php">
+<input type="button" value="Active Directory login" />
+</a>
+
+<a href="newlogin.php">
+<input type="button" value="Standard Login" />
+
+
+
+</html>
