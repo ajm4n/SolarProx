9e473cd2deb98ff5de098e6f7e26e81929db8774
root <arthurjhammond@gmail.com>
2022-03-31 23:16:13 +0000

    modified logout.php so that the login logout flow is a little bit smoother, now redirects to loginoptions instead of the sql login

--- SolarProx/logout.php
+++ SolarProx/logout.php
@@ -8,7 +8,7 @@ $_SESSION = array();
 // Destroy the session.
 session_destroy();
  
-// Redirect to login page
-header("location: newlogin.php");
+// Redirect to login options page
+header("location: loginoptions.php");
 exit;
 ?>
