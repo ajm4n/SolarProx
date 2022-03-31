857ad779842efdf4fdcabfc4e93befc0d9e05a60
gangernv2022 <54276040+gangernv2022@users.noreply.github.com>
2022-03-30 23:29:21 -0400

    Create Player.php

---
+++ SolarProx/Player.php
@@ -0,0 +1,23 @@
+<html>
+
+
+<h1> Player </h1>
+
+<a href="start.sh">
+<input type="button" value="Start" />
+</a>
+
+<a href="stop.sg">
+<input type="button" value="Stop" />
+</a>
+
+<a href="rollback.sh">
+<input type="button" value="Revert" />
+</a>
+
+<a href="reportscore.php">
+<input type="button" value="Report Score" />
+</a>
+
+
+</html>
